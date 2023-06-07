047ab5b98b7ce8d058eaf6e097e30ddecabd4063
Vlas Bieliaiev <vlas.bieliaiev@vizientinc.com>
2023-06-07 23:04:59 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import ChatComponent from "./component/ChatComponent";
+import useWebSocket from "react-use-websocket";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+    chatSection: {
+        width: '100%',
+        height: '80vh'
+    },
+    headBG: {
+        backgroundColor: '#e0e0e0'
+    },
+    borderRight500: {
+        borderRight: '1px solid #e0e0e0'
+    },
+    messageArea: {
+        height: '70vh',
+        overflowY: 'auto'
+    }
+});
+
+const Chat = () => {
+    const classes = useStyles();
+
+    const [messages, setMessages] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    const socket = useWebSocket("ws://192.168.1.2/ws", {
+        onOpen: () => console.log('WebSocket connection opened.'),
+        onClose: () => console.log('WebSocket connection closed.'),
+        shouldReconnect: (closeEvent) => true,
+        onMessage: (event) =>  setMessages([...messages, JSON.parse(event.data)])
+    });
+
+    const onSendingMessage = (data) => {
+        socket.sendMessage(JSON.stringify(data))
+        setMessages([...messages, data])
+    }
+
+    useEffect(() => {
+        const xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", "http://192.168.1.2/history", false); // false for synchronous request
+        xmlHttp.send(null);
+        const response = JSON.parse(xmlHttp.responseText)
+        setMessages(response.data)
+        setLoading(false)
+    }, [])
+
+    if (loading) {
+        return <>LOADING...</>
+    }
+
+    return (
+        <div>
+            <Grid container>
+                <Grid item xs={12} >
+                    <Typography variant="h5" className="header-message">Chat</Typography>
+                </Grid>
+            </Grid>
+            <Grid container component={Paper} className={classes.chatSection}>
+                <Grid item xs={3} className={classes.borderRight500}>
+                    <List>
+                        <ListItem button key="RemySharp">
+                            <ListItemIcon>
+                                <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="John Wick"></ListItemText>
+                        </ListItem>
+                    </List>
+                    <Divider />
+                    <Grid item xs={12} style={{padding: '10px'}}>
+                        <TextField id="outlined-basic-email" label="Search" variant="outlined" fullWidth />
+                    </Grid>
+                    <Divider />
+                    <List>
+                        <ListItem button key="RemySharp">
+                            <ListItemIcon>
+                                <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/1.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="Remy Sharp">Remy Sharp</ListItemText>
+                            <ListItemText secondary="online" align="right"></ListItemText>
+                        </ListItem>
+                        <ListItem button key="Alice">
+                            <ListItemIcon>
+                                <Avatar alt="Alice" src="https://material-ui.com/static/images/avatar/3.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="Alice">Alice</ListItemText>
+                        </ListItem>
+                        <ListItem button key="CindyBaker">
+                            <ListItemIcon>
+                                <Avatar alt="Cindy Baker" src="https://material-ui.com/static/images/avatar/2.jpg" />
+                            </ListItemIcon>
+                            <ListItemText primary="Cindy Baker">Cindy Baker</ListItemText>
+                        </ListItem>
+                    </List>
+                </Grid>
+                <ChatComponent messages={messages} sendMessageHandler={onSendingMessage}/>
+            </Grid>
+        </div>
+    );
 }
 
-export default App;
+export default Chat;
